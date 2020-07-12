989883a9432a31ead517f393e6f0e34b0a933766
datashaman <marlinf@datashaman.com>
2020-07-12 10:03:44 +0200

    wip

--- Makefile
+++ Makefile
@@ -1,54 +1,30 @@
-PHP_MAJOR_VERSION = 7
-PHP_MINOR_VERSION = 3
-EPEL_VERSION = 7
+include php-layer/vars.mak
 
-PHP_PACKAGE = php$(PHP_MAJOR_VERSION)$(PHP_MINOR_VERSION)
-PHP_VERSION = $(PHP_MAJOR_VERSION).$(PHP_MINOR_VERSION)
+.PHONY: build
+build: build-function
 
-PWD := $(shell pwd)
+build-function:
+	sam build
 
-build-PhpLayer:
-	yum update -y
-	rpm --import https://download.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
-	yum install -y \
-		https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(EPEL_VERSION).noarch.rpm
-	yum install -y \
-		libargon2 \
-		libpq \
-		oniguruma \
-		$(PHP_PACKAGE) \
-		$(PHP_PACKAGE)-json \
-		$(PHP_PACKAGE)-mbstring \
-		$(PHP_PACKAGE)-mysql \
-		$(PHP_PACKAGE)-pdo \
-		$(PHP_PACKAGE)-pgsql \
-		$(PHP_PACKAGE)-process \
-		$(PHP_PACKAGE)-xml
-	cd $(ARTIFACTS_DIR) \
-		&& mkdir lib \
-		&& cp \
-			/usr/lib64/libargon2.so* \
-			/usr/lib64/libedit.so* \
-			/usr/lib64/libncurses.so* \
-			/usr/lib64/libonig.so* \
-			/usr/lib64/libpcre.so* \
-			/usr/lib64/libpq.so* \
-			/usr/lib64/libtinfo.so* \
-			lib
-	mkdir $(ARTIFACTS_DIR)/$(PHP_PACKAGE) \
-		&& cd $(ARTIFACTS_DIR)/$(PHP_PACKAGE) \
-		&& mkdir -p lib/php \
-		&& cp -a /usr/lib64/php/${PHP_VERSION}/modules \
-			lib/php
-		&& mkdir bin \
-		&& cp /usr/bin/{phar,php} bin \
-		&& curl -sL https://getcomposer.org/installer | bin/php -- --install-dir=bin/ --filename=composer
-		&& cp $(PWD)/composer.json $(PWD)/composer.lock ./
-		&& bin/composer install
-		&& cp $(PWD)/bootstrap.php $(PWD)/config.php $(PWD)/php.ini ./
-		&& sed -i 's/\${PHP_PACKAGE}/${PHP_PACKAGE}/g' php.ini
-	cd $(ARTIFACTS_DIR) \
-		&& cp $(PWD)/bootstrap .
+build-layer:
+	sam build PhpLayer
+
+deploy: build
+	sam deploy
+
+invoke:
+	sam local invoke
 
 build-HelloWorldFunction:
-	echo hi
+	cp composer.* php.ini $(ARTIFACTS_DIR)
+	cp -a app $(ARTIFACTS_DIR)
+	composer install \
+		--no-ansi \
+		--no-dev \
+		--no-interaction \
+		--no-plugins \
+		--no-progress \
+		--no-scripts \
+		--no-suggest \
+		--optimize-autoloader \
+		--working-dir=$(ARTIFACTS_DIR)
---
+++ php-layer/bootstrap.php
@@ -0,0 +1,186 @@
+<?php
+
+define('LAMBDA_TASK_API', getenv('AWS_LAMBDA_RUNTIME_API'));
+define('LAMBDA_TASK_HANDLER', getenv('_HANDLER'));
+define('LAMBDA_TASK_ROOT', getenv('LAMBDA_TASK_ROOT'));
+
+require_once LAMBDA_TASK_ROOT . '/vendor/autoload.php';
+
+use DI\ContainerBuilder;
+use GuzzleHttp\Client;
+use Psr\Container\ContainerInterface;
+
+class RuntimeHandler
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $requestId = '';
+
+    public function __construct()
+    {
+        $this->createClient();
+
+        try {
+            $this->configureErrorReporting();
+            $this->buildContainer();
+        } catch (Throwable $exception) {
+            $this->error(
+                'Error initializing handler',
+                [
+                    'message' => $exception->getMessage(),
+                ]
+            );
+            $this->postError($exception);
+        }
+}
+
+    public function __invoke(): void
+    {
+        $this->info('Invoke handler event loop');
+
+        while (true) {
+            try {
+                $event = $this->getNextInvocation();
+                $context = [];
+                $response = $this->container->call(
+                    LAMBDA_TASK_HANDLER,
+                    [
+                        'event' => $event,
+                    ]
+                );
+                $this->postResponse($response);
+            } catch (Throwable $exception) {
+                $this->error(
+                    'Error processing event',
+                    [
+                        'event' => $event ?? [],
+                        'response' => $response ?? '',
+                    ]
+                );
+                $this->postError($exception);
+            }
+        }
+    }
+
+    private function configureErrorReporting(): void
+    {
+        $this->info('Configure error reporting');
+
+        error_reporting(
+            E_ALL &
+            ~E_USER_DEPRECATED &
+            ~E_DEPRECATED &
+            ~E_STRICT &
+            ~E_NOTICE
+        );
+    }
+
+    private function buildContainer(): void
+    {
+        $this->info('Build container');
+
+        $containerBuilder = new ContainerBuilder();
+
+        if ($configPath = $this->taskPath('config.php')) {
+            $containerBuilder->addDefinitions($configPath);
+        }
+
+        $this->container = $containerBuilder->build();
+    }
+
+    private function createClient(): void
+    {
+        $this->info('Create client');
+
+        $this->client = new Client(
+            [
+                'base_uri' => 'http://' . LAMBDA_TASK_API . '/2018-06-01/',
+            ]
+        );
+    }
+
+    private function getNextInvocation(): array
+    {
+        $this->info('Get next invocation');
+
+        $response = $this->client->get('runtime/invocation/next');
+        $this->requestId = $response->getHeader('lambda-runtime-aws-request-id')[0];
+
+        return json_decode($response->getBody(), true);
+    }
+
+    private function postResponse(array $response): void
+    {
+        $this->info('Post response');
+
+        $this->client->post(
+            "runtime/invocation/{$this->requestId}/response",
+            [
+                'json' => $response,
+            ]
+        );
+    }
+
+    private function postError(Throwable $exception): void
+    {
+        if ($this->client) {
+            $this->info('Post error');
+
+            $path = $this->requestId
+                ? "runtime/invocation/{$this->requestId}/error"
+                : 'runtime/init/error';
+
+            $error = [
+                'errorMessage' => $exception->getMessage() . ' ' . $exception->getFile() . ':' . $exception->getLine(),
+                'errorType' => get_class($exception),
+            ];
+
+            $this->client->post(
+                $path,
+                [
+                    'json' => $error,
+                    'headers' => [
+                        'Lambda-Runtime-Function-Error-Type' => 'Unhandled',
+                    ],
+                ]
+            );
+        }
+
+        if (!$this->requestId) {
+            exit(1);
+        }
+    }
+
+    private function taskPath(string $path = '')
+    {
+        return realpath(LAMBDA_TASK_ROOT . '/' . $path);
+    }
+
+    private function log($handle, string $message, array $context = []): void
+    {
+        fwrite($handle, $message . ($context ? (' ' . json_encode($context)) : '') . PHP_EOL);
+    }
+
+    private function error(string $message, array $context = []): void
+    {
+        $this->log(STDERR, $message, $context);
+    }
+
+    private function info(string $message, array $context = []): void
+    {
+        $this->log(STDOUT, $message, $context);
+    }
+}
+
+(new RuntimeHandler())();
